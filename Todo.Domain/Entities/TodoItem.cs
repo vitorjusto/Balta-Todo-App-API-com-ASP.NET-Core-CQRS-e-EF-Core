3d5dfc8ecd3675d173a0d423c8c20b80b89f50f9
vitorjusto <vitorhugojusto1@gmail.com>
2022-12-16 10:49:46 -0300

    Entidade TodoItem

---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class TodoItem : Entity
+    {
+        protected TodoItem(string title, DateTime date, string user)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public string Title {get; private set;}
+        public bool Done {get; private set;}
+        public DateTime Date {get; private set;}
+        public string User {get; private set;}
+
+        public void MarkAsDone()
+            => Done = true;
+        public void MarkAsUndone()
+            => Done = false;
+
+        public void UpdateTitle(string title)
+            => Title = title;
+    }
+}
