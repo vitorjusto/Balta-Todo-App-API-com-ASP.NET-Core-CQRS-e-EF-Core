4afb16b0dbc21bc1034c80b171eb760345106593
Vitor Justo <vitor.justo@inovafarma.com.br>
2023-01-05 10:13:35 -0300

    implementa startup

---
+++ Todo.Domain.Api/Startup.cs
@@ -0,0 +1,35 @@
+
+namespace Todo.Domain.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
