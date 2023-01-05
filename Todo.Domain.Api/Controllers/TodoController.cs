7b6dddbbc024a8cc40e261c18e6ccd670266b0c6
Vitor Justo <vitor.justo@inovafarma.com.br>
2023-01-05 16:25:14 -0300

    Testando a api

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,7 +1,9 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Api.Controllers
 {
@@ -9,6 +11,15 @@ namespace Todo.Domain.Api.Controllers
     [Route("v1/todos")]
     public class TodoController : ControllerBase
     {
+        [Route("")]
+        [HttpPost]
+        public IEnumerable<TodoItem> GetAll(
+            [FromServices]ITodoRepository repository
+        )
+        {
+            return repository.GetAll("vitor");
+        }
+
         [Route("")]
         [HttpPost]
         public GenericCommandResult Create(
