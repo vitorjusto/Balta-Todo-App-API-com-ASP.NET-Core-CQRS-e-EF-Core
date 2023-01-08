25fca999d448f4713409c1b2919976d608affa69
vitorjusto <vitorhugojusto1@gmail.com>
2023-01-08 16:17:59 -0300

    configurando o sql server

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -12,7 +12,7 @@ namespace Todo.Domain.Api.Controllers
     public class TodoController : ControllerBase
     {
         [Route("")]
-        [HttpPost]
+        [HttpGet]
         public IEnumerable<TodoItem> GetAll(
             [FromServices]ITodoRepository repository
         )
@@ -20,10 +20,79 @@ namespace Todo.Domain.Api.Controllers
             return repository.GetAll("vitor");
         }
 
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone(
+            [FromServices]ITodoRepository repository
+        )
+        {
+            return repository.GetAllDone("vitor");
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone(
+            [FromServices]ITodoRepository repository
+        )
+        {
+            return repository.GetAllUndone("vitor");
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllToday(
+            [FromServices]ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod("vitor", DateTime.Now.Date, true);
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndoneToday(
+            [FromServices]ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod("vitor", DateTime.Now.Date, false);
+        }
+
         [Route("")]
         [HttpPost]
         public GenericCommandResult Create(
             [FromBody]CreateTodoCommand command,
+            [FromServices]TodoHandler handler
+        )
+        {
+            command.User = "Vitor";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("")]
+        [HttpPut]
+        public GenericCommandResult Update(
+            [FromBody]UpdateTodoCommand command,
+            [FromServices]TodoHandler handler
+        )
+        {
+            command.User = "Vitor";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone(
+            [FromBody]MarkTodoAsDoneCommand command,
+            [FromServices]TodoHandler handler
+        )
+        {
+            command.User = "Vitor";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone(
+            [FromBody]MarkTodoAsUndoneCommand command,
             [FromServices]TodoHandler handler
         )
         {
